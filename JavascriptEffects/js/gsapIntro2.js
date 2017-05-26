c90fe3160d0b13b004057749e7146f6f7739886a
mahana <user@mahana-2.local>
2017-05-26 11:54:48 +0200

    gsap n2

--- JavascriptEffects/js/gsapIntro2.js
+++ JavascriptEffects/js/gsapIntro2.js
@@ -1,3 +1,5 @@
 TweenMax.from(".logo", 0.5, {opacity:0, scale:0, ease:Bounce.easeInOut});
 
 TweenMax.staggerFrom(".box", 0.5, {opacity:0, y:200, scale:2, delay:0.5}, 0.2);
+
+TweenMax.to("#header", 1, {opacity:1, left:900});
